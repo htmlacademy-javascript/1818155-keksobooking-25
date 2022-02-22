8f8fa0fd1e01a639cb37d5e1743e7f485936b643
LadyMelamori <boychenko.tatyana@gmail.com>
2022-02-22 15:43:11 +0300

    Функция получения случайного дробного числа из диапазона

--- js/main.js
+++ js/main.js
@@ -12,4 +12,27 @@ const getRandomIntFromRange = (from, to) => {
   return Math.floor(rand * (difference + 1)) + fromRounded;
 };
 
-getRandomIntFromRange(1, 4);
+const getRandomFloatFromRange = (from, to, precision) => {
+  if (from < 0 || to < 0 || to < from) {
+    throw 'Invalid parameters';
+  }
+
+  const precisionMultiplier = Math.pow(10, precision);
+
+  // танцы с бубном, чтобы учитывать только переданное количество знаков после запятой в границах диапазона
+  // и избавиться таким образом от влияния шальной единички в конце float'а на округление
+  const fromPrecision = from.toString().split('.')[1].length || 0;
+  const fromShiftedPrecision = Math.max(fromPrecision - precision, 0);
+  const fromShiftedPrecisionMultiplier = Math.pow(10, fromShiftedPrecision);
+
+  const toPrecision = to.toString().split('.')[1].length || 0;
+  const toShiftedPrecision = Math.max(toPrecision - precision, 0);
+  const toShiftedPrecisionMultiplier = Math.pow(10, toShiftedPrecision);
+
+  const fromShifted = Math.ceil(Math.round(from * precisionMultiplier * fromShiftedPrecisionMultiplier) / fromShiftedPrecisionMultiplier);
+  const toShifted = Math.floor(Math.round(to * precisionMultiplier * toShiftedPrecisionMultiplier) / toShiftedPrecisionMultiplier);
+
+  return (getRandomIntFromRange(fromShifted, toShifted) / precisionMultiplier).toFixed(precision);
+};
+
+getRandomFloatFromRange(1.1, 2.6, 3);
